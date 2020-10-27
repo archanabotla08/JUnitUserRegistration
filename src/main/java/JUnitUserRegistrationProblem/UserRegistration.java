cc50d206b90852710828db4171544e1734267379
Nisha271 <bukkenisha@gmail.com>
2020-10-27 20:54:16 +0530

    [Archana] Added: UC1 with user registartion for first name

--- src/main/java/JUnitUserRegistrationProblem/UserRegistration.java
+++ src/main/java/JUnitUserRegistrationProblem/UserRegistration.java
@@ -4,4 +4,11 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	
+	private static final String FIRSTNAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
+	
+	public boolean UserRegistrationFirstName(String firstName) {
+		
+		return Pattern.matches(FIRSTNAME_PATTERN, firstName);
+	}
+	
 }
