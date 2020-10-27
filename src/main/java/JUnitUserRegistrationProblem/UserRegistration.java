0f01ef9e731be301839e4f5b93236d8608b44221
Nisha271 <bukkenisha@gmail.com>
2020-10-27 21:02:15 +0530

    [Archana] Added: UC3 for email validation

--- src/main/java/JUnitUserRegistrationProblem/UserRegistration.java
+++ src/main/java/JUnitUserRegistrationProblem/UserRegistration.java
@@ -6,6 +6,7 @@ public class UserRegistration {
 	
 	private static final String FIRSTNAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
 	private static final String LASTNAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
+	private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([.+_-]{1}[a-zA-Z0-9]{3,}){0,2}\\\\@[a-zA-Z0-9]+([.]{1,2}[a-z]{2,}){0,2}";
 	
 	public boolean UserRegistrationFirstName(String firstName) {
 		
@@ -15,5 +16,10 @@ public class UserRegistration {
 		
 		return Pattern.matches(LASTNAME_PATTERN, lastName);
 	}
+	public boolean UserRegistrationEmailName(String email) {
+		
+		return Pattern.matches(EMAIL_PATTERN, email);
+	}
+	
 	
 }
