f192e4d58805fef986bb5d501b96854421e5defd
Nisha271 <bukkenisha@gmail.com>
2020-10-27 21:32:44 +0530

    [Archana] Added: UC4 for user mobile number validation

--- src/main/java/JUnitUserRegistrationProblem/UserRegistration.java
+++ src/main/java/JUnitUserRegistrationProblem/UserRegistration.java
@@ -7,6 +7,7 @@ public class UserRegistration {
 	private static final String FIRSTNAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
 	private static final String LASTNAME_PATTERN = "^[A-Z]{1}[a-z]{2,}";
 	private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([.+_-]{1}[a-zA-Z0-9]{3,}){0,2}\\\\@[a-zA-Z0-9]+([.]{1,2}[a-z]{2,}){0,2}";
+	private static final String MOBILENUMBER_PATTERN = "^(91)[7-9][0-9]{9}$}";
 	
 	public boolean UserRegistrationFirstName(String firstName) {
 		
@@ -16,9 +17,13 @@ public class UserRegistration {
 		
 		return Pattern.matches(LASTNAME_PATTERN, lastName);
 	}
-	public boolean UserRegistrationEmailName(String email) {
+	public boolean UserRegistrationEmail(String email) {
 		
 		return Pattern.matches(EMAIL_PATTERN, email);
+	}
+	public boolean UserRegistrationMobileNumber(String number) {
+		
+		return Pattern.matches(MOBILENUMBER_PATTERN, number);
 	}
 	
 	
